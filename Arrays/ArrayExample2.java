83dada2841e9dcadb1e387c382c8442d65f758bf
Uzair Ahmad <uzairahmad009090@gmail.com>
2026-07-16 05:47:52 -0700

    Add ArrayExample2.java

---
+++ Arrays/ArrayExample2.java
@@ -0,0 +1,6 @@
+ // Example 2
+        System.out.println("\n===== Example 2: String Array =====");
+        String[] students = {"Ali", "Ahmed", "Usman", "Sara"};
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i]);
+        }
