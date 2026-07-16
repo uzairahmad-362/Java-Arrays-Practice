c56d596c6b4c4ac858b3c2abf1f3562ebd1ff531
Uzair Ahmad <uzairahmad009090@gmail.com>
2026-07-16 05:48:48 -0700

    Add ArrayExample3.java

---
+++ Arrays/ArrayExample3.java
@@ -0,0 +1,8 @@
+// Example 3
+        System.out.println("\n===== Example 3: Sum =====");
+        int[] numbers = {10, 20, 30, 40, 50};
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println("Sum = " + sum);
