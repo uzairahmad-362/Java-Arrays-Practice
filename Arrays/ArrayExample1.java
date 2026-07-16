c2e02d9f396a70f4fcc770f1e6416bf2ae729e78
Uzair Ahmad <uzairahmad009090@gmail.com>
2026-07-16 05:46:18 -0700

    Add Array Example 1
    Uploaded first Java array practice program.

---
+++ Arrays/ArrayExample1.java
@@ -0,0 +1,6 @@
+ // Example 1
+        System.out.println("===== Example 1: Integer Array =====");
+        int[] marks = {80, 75, 90, 88, 95};
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
